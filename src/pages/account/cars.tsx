bac852febf515b0b931bb103ce9372f758c0ce3c
Ilya <sange0337@gmail.com>
2025-04-17 13:11:43 +0800

    add skeleton for vehicles

--- src/pages/account/cars.tsx
+++ src/pages/account/cars.tsx
@@ -5,29 +5,33 @@ import AddVehicleDialog from '@/components/molecules/account/add-vehicle-dialog'
 import DeleteVehicleDialog from '@/components/molecules/account/delete-vehicle-dialog';
 import { useUserContext } from '@/lib/hooks/context/use-user-context';
 import { Car } from '@/lib/types/user';
+import { range } from '@/lib/utils/range';
 import { useState } from 'react';
 
 export default function AccountCars() {
-
     const { user } = useUserContext();
 
     const [selectedVehicle, setSelectedVehicle] = useState<Car | null>(null);
     const [showAddVehicleDialog, setShowAddVehicleDialog] = useState(false);
 
-    if (user == null) return null;
-
     return (
         <section className="pt-6">
             <div>
                 <ul className="space-y-3 mb-3.5 md:space-y-5">
-                    {user.cars.map((car, idx) => (
-                        <AccountVehicle
-                            key={`${car.id}-${idx}`}
-                            vehicle={car}
-                            onClick={() => setSelectedVehicle(car)}
-                            idx={idx + 1}
-                        />
-                    ))}
+                    {user != null
+                        ? user.cars.map((car, idx) => (
+                              <AccountVehicle
+                                  key={`${car.id}-${idx}`}
+                                  vehicle={car}
+                                  onClick={() => setSelectedVehicle(car)}
+                                  idx={idx + 1}
+                              />
+                          ))
+                        : range(1, 5).map((index) => (
+                              <AccountVehicleSkeleton
+                                  key={`vehicle-skeleton-${index}`}
+                              />
+                          ))}
                 </ul>
 
                 <AccountAddBtn onClick={() => setShowAddVehicleDialog(true)} />
@@ -54,5 +58,13 @@ export default function AccountCars() {
                 />
             </DialogLayout>
         </section>
+    );
+}
+
+function AccountVehicleSkeleton() {
+    return (
+        <li className="w-full px-6 py-3 bg-gray-400 text-transparent animate-pulse flex items-center gap-5 rounded-full lg:py-5">
+            <div className="h-5 shrink-0 lg:h-8">Loading</div>
+        </li>
     );
 }
