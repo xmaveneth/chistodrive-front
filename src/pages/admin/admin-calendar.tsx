04c4af48a4cc3c05c7dabe546be00a5be2e10dad
Ilya <sange0337@gmail.com>
2025-05-29 22:18:13 +0800

    make table for calendar script versions

--- src/pages/admin/admin-calendar.tsx
+++ src/pages/admin/admin-calendar.tsx
@@ -1,8 +1,9 @@
 import AdminSkeleton from '@/components/atoms/admin-skeleton';
-import TableHead from '@/components/molecules/admin/table-head';
+import PrimaryBtn from '@/components/atoms/primary-btn';
 import { DatePicker } from '@/components/organisms/admin/date-picker';
 import { useCalendarData } from '@/lib/hooks/calendar/use-calendar-data';
 import { ScheduleVersion } from '@/lib/types/schedule';
+import { cn } from '@/lib/utils';
 import {
     formatDateForScripts,
     formatDateToString,
@@ -24,13 +25,14 @@ export default function AdminCalendar() {
         setDate(newDate);
     }
 
-    if (isLoading) return <div>Loading...</div>;
-
     console.log(calendarData);
     return (
         <div>
             <DatePicker value={date} onChange={selectDate} />
-            <InitialScriptVersions versions={calendarData?.versions} isLoading={isLoading} />
+            <InitialScriptVersions
+                versions={calendarData?.versions}
+                isLoading={isLoading}
+            />
         </div>
     );
 }
@@ -46,17 +48,17 @@ function InitialScriptVersions({
 }: InitialScriptVersionsProps) {
     return (
         <div className="overflow-auto scrollbar-hidden text-xs sm:text-base">
-            <TableHead gridClass="grid-cols-[60px_1fr_1fr_1fr_60px]">
+            <TableHead>
                 <div>Название</div>
                 <div>Создан</div>
-                <div>Статус</div>
                 <div></div>
             </TableHead>
 
             {isLoading || versions == null ? (
-                <AdminSkeleton />
-            ) : (
-                versions &&
+                <div className="mx-4">
+                    <AdminSkeleton className="mx-auto !w-240 !sm:w-342" />
+                </div>
+            ) : versions && versions.length > 0 ? (
                 versions.map((version, idx) => (
                     <div key={`version-${idx}`}>
                         <InitialScriptVersionRow
@@ -66,6 +68,8 @@ function InitialScriptVersions({
                         />
                     </div>
                 ))
+            ) : (
+                <div>На эту дату слоты еще не сформированы</div>
             )}
         </div>
     );
@@ -83,22 +87,44 @@ function InitialScriptVersionRow({
     onClick,
 }: InitialScriptVersionRow) {
     return (
-        <div className="w-180 sm:w-282 text-center grid grid-cols-[60px_1fr_1fr_1fr_60px] divide-x-1 mx-4 divide-white/20 border-y border-white/20">
-            <div className="py-3 cursor-pointer sticky left-0 z-10 flex items-center gap-2 justify-center bg-background text-btn-bg">
-                {index}
+        <div className="mx-auto w-max">
+            <div className="w-150 sm:w-252 text-center grid grid-cols-[60px_1fr_1fr_160px] divide-x-1 mx-4 divide-white/20 border-y border-white/20">
+                <div className="py-3 cursor-pointer sticky left-0 z-10 flex items-center gap-2 justify-center bg-background text-btn-bg">
+                    {index}
+                </div>
+                <div className="py-3 flex items-center justify-center">
+                    <span className="mx-auto">{version.name}</span>
+                </div>
+                <div className="py-3 flex items-center justify-center">
+                    {formatDateForScripts(version.created_at)}
+                </div>
+                <div className="py-3 flex items-center justify-center">
+                    {' '}
+                    <PrimaryBtn onClick={onClick} className="text-sm">
+                        Применить
+                    </PrimaryBtn>
+                </div>
             </div>
-            <div className="py-3 flex items-center justify-center">
-                <span className="mr-auto">{version.name}</span>
+        </div>
+    );
+}
+
+type TableHeadProps = {
+    children: React.ReactNode;
+};
+
+function TableHead({ children }: TableHeadProps) {
+    return (
+        <div
+            className={cn(
+                'w-158 sm:w-260 mx-auto grid-cols-[60px_1fr_1fr_160px] sticky top-0 z-20 text-center grid bg-light-bg p-4 rounded-full divide-x-1 divide-white/20 mb-4'
+            )}
+        >
+            <div className="sticky left-0 z-20 bg-light-bg rounded-l-xl  text-btn-bg">
+                №
+                <span className="absolute left-0 top-0 bottom-0  w-10 bg-light-bg -z-10"></span>
             </div>
-            <div className="py-3 flex items-center justify-center">
-                {formatDateForScripts(version.created_at)}
-            </div>
-            <div className="py-3 flex items-center justify-center">
-                {' '}
-                <button onClick={onClick} className="cursor-pointer">
-                    Apply
-                </button>
-            </div>
+            {children}
         </div>
     );
 }
