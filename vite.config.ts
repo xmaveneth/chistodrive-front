d8ee0a8ea248ceb3efa952637dec349f43a0de36
Ilya <sange0337@gmail.com>
2025-06-07 22:23:48 +0800

    add node types

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 // import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
-// import path from "path";
+import path from "path";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
-        // alias: {
-        // "@": path.resolve(__dirname, "./src"),
-        // },
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
 });
