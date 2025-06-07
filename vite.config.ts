b86ae5dd253aed65f0faed568375ea47990cdcbc
Ilya <sange0337@gmail.com>
2025-06-07 22:17:08 +0800

    remove path directory from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 // import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 // import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
-        alias: {
-            // "@": path.resolve(__dirname, "./src"),
-            '@': resolve(__dirname, './src'),
-        },
+        // alias: {
+        // "@": path.resolve(__dirname, "./src"),
+        // },
     },
 });
