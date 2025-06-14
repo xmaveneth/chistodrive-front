05f95d9e1713cb88a496e0660a72248995f5e2de
Ilya <sange0337@gmail.com>
2025-06-14 22:04:27 +0800

    add file type validation to leave review form

--- src/components/molecules/account/add-review-dialog.tsx
+++ src/components/molecules/account/add-review-dialog.tsx
@@ -4,9 +4,9 @@ import PrimaryBtn from '@/components/atoms/primary-btn';
 import { usePostReview } from '@/lib/hooks/reviews/use-post-review';
 import { Appointment } from '@/lib/types/user';
 import { formatDateForScripts, formatTimeToHHMM } from '@/lib/utils/format-date';
-import notify from '@/lib/utils/notify';
 import { Textarea } from '@headlessui/react';
 import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 const MAX_FILE_SIZE_MB = 1024 * 1024 * 48;
 
@@ -22,13 +22,17 @@ export default function AddReviewDialog({
     const [files, setFiles] = useState<File[]>([]);
     const [comment, setComment] = useState('');
     const [rating, setRating] = useState<number | null>(0);
+    const [ratingError, setRatingError] = useState("");
+    const [commentError, setCommentError] = useState("");
+    const [fileError, setFileError] = useState("");
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const selected = e.target.files;
         if (!selected) return;
+        setFileError('');
 
         if (files.length + selected.length > 5) {
-            notify("Выберите максимум 5 фото");
+            setFileError("Выберите максимум 5 фото");
             return;
         }
 
@@ -41,27 +45,34 @@ export default function AddReviewDialog({
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setFileError("");
+        setCommentError("");
+        setRatingError("");
 
         if (comment.length > 480) {
-            notify("Пожалуйста, оставьте более короткий комментарий")
+            setCommentError("Пожалуйста, оставьте более короткий комментарий");
             return;
         }
 
         if (comment.length === 0) {
-            notify("Пожалуйста, оставьте комментарий")
+            setCommentError("Пожалуйста, оставьте комментарий");
             return;
         }
         if (rating == null || rating == 0) {
-            notify("Пожалуйста, дайте оценку автомойке для оставления комментария")
+            setRatingError("Пожалуйста, дайте оценку автомойке для оставления комментария");
             return;
         }
 
-        files.forEach(file => {
+        for (const file of files) {
             if (file.size > MAX_FILE_SIZE_MB) {
-                notify("Размер файла не должен превышать 50 МБ")
+                setFileError("Размер файла не должен превышать 50 МБ");
                 return;
             }
-        })
+            if (!file.type.startsWith('image/')) {
+                setFileError("Пожалуйста, загрузите фото форматов webp, png, или jpeg");
+                return;
+            }
+        }
         const formData = new FormData();
         formData.append('appointment_id', String(entry.appointment_id));
         formData.append('rating', rating.toString());
@@ -83,7 +94,7 @@ export default function AddReviewDialog({
             </div>
 
             <div className="space-y-2 my-8">
-                <div className='rounded-lg bg-white/10 w-full px-4 py-2 flex items-center justify-between gap-2'>
+                <div className={cn('rounded-lg bg-white/10 w-full px-4 py-2 flex items-center justify-between gap-2', ratingError.length > 0 && 'border border-red-500')}>
                     <div className='text-sm'>Оцените это место</div>
                     <Rating
                         name="simple-controlled"
@@ -93,9 +104,14 @@ export default function AddReviewDialog({
                         }}
                     />
                 </div>
-                <Textarea onChange={(e) => setComment(e.target.value)} value={comment} placeholder='Напишите комментарий' className="rounded-lg bg-white/10 w-full px-4 py-2 min-h-30" maxLength={480} />
 
-                <div className='rounded-lg bg-white/10 w-full p-2'>
+                {ratingError.length > 0 && <p className='text-sm text-red-500'>{ratingError}</p>}
+
+                <Textarea onChange={(e) => setComment(e.target.value)} value={comment} placeholder='Напишите комментарий' className={cn("rounded-lg bg-white/10 w-full px-4 py-2 min-h-30", commentError.length > 0 && "border border-red-400")} maxLength={480} />
+
+                {commentError.length > 0 && <p className='text-sm text-red-500'>{commentError}</p>}
+
+                <div className={cn('rounded-lg bg-white/10 w-full p-2', fileError.length > 0 && 'border border-red-500')}>
 
                     <div className="flex items-center justify-center w-full">
                         <label htmlFor="review-files" className="flex flex-col items-center justify-center w-full h-30 border-2 border-btn-bg border-dotted rounded-lg cursor-pointer">
@@ -130,6 +146,7 @@ export default function AddReviewDialog({
                     )}
 
                 </div>
+                {fileError.length > 0 && <p className='text-sm text-red-500'>{fileError}</p>}
             </div>
 
             <PrimaryBtn type="submit" disabled={isPending} className="w-full">
