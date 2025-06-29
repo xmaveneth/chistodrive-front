4b6e33a169a49971f6ae1a00cdc0ffccae5c7b29
Ilya <sange0337@gmail.com>
2025-06-29 14:22:02 +0800

    make visual effect of deactivated and activated calendar day

--- src/pages/admin/admin-calendar.tsx
+++ src/pages/admin/admin-calendar.tsx
@@ -64,12 +64,12 @@ export default function AdminCalendar() {
                         <Switch
                             checked={enabled}
                             onChange={handleDayToggle}
-                            className="group ml-auto mt-2 relative flex h-7 w-14 cursor-pointer rounded-full bg-white/10 p-1 ease-in-out focus:not-data-focus:outline-none data-checked:bg-white/10 data-focus:outline data-focus:outline-white"
+                            className="group ml-auto mt-2 relative flex h-7 w-14 cursor-pointer rounded-full p-1 ease-in-out outline-1 outline-gray-300/40 data-checked:outline-btn-bg"
                             disabled={pendingActivateDay || pendingDeactivateDay}
                         >
                             <span
                                 aria-hidden="true"
-                                className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out group-data-checked:translate-x-7"
+                                className="pointer-events-none inline-block size-5 translate-x-0 rounded-full bg-gray-300/40 shadow-lg ring-0 transition duration-200 ease-in-out group-data-checked:translate-x-7 group-data-checked:bg-btn-bg"
                             />
                         </Switch>
 
