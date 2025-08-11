2c158565d380a5207de5df4c5694164eaafcb7a9
Ilya <sange0337@gmail.com>
2025-08-11 18:55:48 +0800

    revert to swc compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-// import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 
