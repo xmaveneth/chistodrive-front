21523fa1ac0cf0f2d5707d0986cdb72f8222fbca
Neoscripter <sange0337@gmail.com>
2025-08-20 13:58:23 +0800

    change footer

--- src/components/atoms/footer-social.tsx
+++ src/components/atoms/footer-social.tsx
@@ -7,7 +7,7 @@ export default function FooterSocial({ html, url }: FooterSocialProps) {
         <a
             href={url}
             target="_blank"
-            className="flex items-center justify-center border shrink-0 size-8 rounded-full border-white/20 transition-colors duration-200 ease-in hover:bg-btn-bg/20"
+            className="flex items-center justify-center border shrink-0 size-8 xs:size-10 md:size-12 rounded-full border-white/20 transition-colors duration-200 ease-in hover:bg-btn-bg/20"
             dangerouslySetInnerHTML={{ __html: html }}
         ></a>
     );
--- src/components/organisms/shared/footer.tsx
+++ src/components/organisms/shared/footer.tsx
@@ -2,6 +2,7 @@ import logo from '@/assets/images/home/logo.webp';
 import tgSvg from '@/assets/svgs/tg.svg?raw';
 import vkSvg from '@/assets/svgs/vk.svg?raw';
 import FooterSocial from '@/components/atoms/footer-social';
+import { EnvelopeIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
 
 export default function Footer() {
@@ -9,46 +10,11 @@ export default function Footer() {
         <footer className="primary-px pb-5 sm:pb-8 xl:pt-3.5 xl:pb-10">
             <nav
                 aria-label="Основная навигация"
-                className="flex items-center py-2 xs:px-4.5 px-3 sm:px-9 sm:py-3 mb-4 md:mb-0 justify-between rounded-full bg-input-bg text-xs sm:text-sm gap-1 sm:gap-3 xl:gap-6"
+                className="space-y-3 xs:space-y-4 lg:space-y-6 xl:space-y-8 py-4 px-6 sm:py-4.5 md:py-6 md:px-8 mb-4 md:mb-0 xl:py-10 xl:px-10 rounded-3xl bg-input-bg text-xs xs:text-sm md:text-base xl:text-lg"
             >
-                <Link
-                    to="/"
-                    className="shrink-0 w-22 sm:w-32.25 hidden xs:block"
-                >
-                    <img
-                        src={logo}
-                        alt="Chisto.drive логотип"
-                        className="object-contain"
-                    />
-                </Link>
+                <FooterTop />
 
-                <div className="hidden sm:flex items-center gap-1.5 xs:gap-2.5">
-                    <FooterSocial url="https://vk.com/chisto.drive" html={vkSvg} />
-                    <FooterSocial url="https://t.me/chisto_drive" html={tgSvg} />
-                </div>
-
-                <div className="flex items-center gap-3 sm:gap-6 md:mr-auto">
-                    <Link
-                        to="/rules"
-                        className="block text-center hover:underline underline-offset-4"
-                    >
-                        Пользовательское соглашение
-                    </Link>
-                </div>
-
-                <Link
-                    to="/policy"
-                    className="text-text-muted mr-2 text-center hover:underline underline-offset-4"
-                >
-                    Политика конфиденциальности
-                </Link>
-
-                <div
-                    className="text-text-muted text-sm hidden md:block"
-                    aria-hidden={true}
-                >
-                    ©2025 CHISTO.DRIVE
-                </div>
+                <FooterBottom />
             </nav>
 
             <div
@@ -58,5 +24,85 @@ export default function Footer() {
                 ©2025 CHISTO.DRIVE
             </div>
         </footer>
+    );
+}
+
+function FooterTop() {
+    return (
+        <div className="flex items-center flex-wrap gap-4 xs:gap-6 lg:justify-between">
+            <Link
+                to="/"
+                className="shrink-0 w-32 xs:w-42.25 md:w-60 xl:w-80"
+            >
+                <img
+                    src={logo}
+                    alt="Chisto.drive логотип"
+                    className="object-contain"
+                />
+            </Link>
+
+            <div className="flex items-center gap-1.5 xs:gap-2.5 ml-auto lg:order-2 lg:ml-0 lg:basis-full xl:mt-3">
+                <FooterSocial
+                    url="https://vk.com/chisto.drive"
+                    html={vkSvg}
+                />
+                <FooterSocial
+                    url="https://t.me/chisto_drive"
+                    html={tgSvg}
+                />
+            </div>
+            <div className="flex items-center gap-3 xs:gap-6 md:gap-8 basis-full lg:basis-auto">
+                <Link
+                    to="/rules"
+                    className="block hover:underline underline-offset-4"
+                >
+                    Пользовательское соглашение
+                </Link>
+                <Link
+                    to="/"
+                    className="hover:underline underline-offset-4 whitespace-nowrap"
+                >
+                    О сервисе
+                </Link>
+            </div>
+
+            <div
+                className="text-text-muted text-sm hidden lg:block lg:text-lg"
+                aria-hidden={true}
+            >
+                ©2025 CHISTO.DRIVE
+            </div>
+        </div>
+    );
+}
+
+function FooterBottom() {
+    return (
+        <div className="flex lg:flex-row lg:items-center lg:justify-between flex-col gap-3 xs:gap-4 text-text-muted">
+            <div className="">
+                ИП Голованов Кирилл Антонович <br /> ОГРНИП 325565800075437 / ИНН
+                561011278636
+            </div>
+
+            <div className="">
+                <Link
+                    to="/policy"
+                    className="text-text-muted hover:underline underline-offset-4"
+                >
+                    Политика конфиденциальности
+                </Link>
+            </div>
+
+            <div>
+                <a
+                    href="mailto:chisto.drive@yandex.ru"
+                    target='_blank'
+                    className="flex items-center gap-2 hover:underline underline-offset-4"
+                >
+                    <EnvelopeIcon className="text-btn-bg size-4 xs:size-5" />
+                    chisto.drive@yandex.ru
+                </a>
+            </div>
+        </div>
     );
 }
